a580a81eca203c92e2fd7da5bd1f31de65eac69c
jaayush27 <aayushjoshi2709@gmail.com>
2022-02-10 17:54:21 +0530

    authentication done

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express = require('express');
-const App = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const { append } = require('express/lib/response');
+
+const ioClient               = require('socket.io-client'),
+      express                = require('express'),
+      App                    = express(),
+      mongoose               = require('mongoose'),
+      bodyParser             = require('body-parser'),
+      { Server }             = require('socket.io'),
+      passport               = require('passport'),
+      { createServer }       = require("http"),
+      localStrategy          = require('passport-local'),
+      passportLocalMongoose  = require('passport-local-mongoose');
+
+App.set("view engine","ejs");
+// set app to user body parser
+App.use(bodyParser({extended:true}));
+// adding public dir to the app
+App.use(express.static(__dirname + "/public"));
+
 // creating user model with mongoose
 mongoose.connect(process.env.databaseURL);
 const userSchema = mongoose.Schema({
@@ -9,49 +24,76 @@ const userSchema = mongoose.Schema({
     lastName: String,
     email: String,
     password: String,
-    userName: String
+    username: String
 });
+// passport configuration
+userSchema.plugin(passportLocalMongoose);
 var User = mongoose.model("User",userSchema);
+App.use(require('express-session')({
+    secret:"hello world",
+    resave:false,
+    saveUninitialized: false
+}));
+App.use(passport.initialize());
+App.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new localStrategy(User.authenticate()));
 // creating message model with mongoose
 const messageSchema = mongoose.Schema({
-    form: String,
+    from: String,
     to: String,
     time: String,
     message: String
 });
-var message = mongoose.model("Message",messageSchema);
-
-App.set("view engine","ejs");
-
-// set app to user body parser
-App.use(bodyParser({extended:true}));
-// adding public dir to the app
-App.use(express.static(__dirname + "/public"));
+var Message = mongoose.model("Message",messageSchema);
 // root route
 App.get("/",function(req,res){
     res.render('index')
 })
 // new user route
-App.get("/user/new",function(req,res){
+App.get("/register",function(req,res){
     res.render("./user/new");
 })
 // create user route
-App.post("/user",function(req,res){
+App.post("/register",function(req,res){
     //adding the user to database
-    User.create(req.body.user,function(error,user){
-        if(error) console.log(error);
-        else {
+    var user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        username: req.body.username
+    }
+    User.register(new User(user),req.body.password,function(error,user){
+        if(error){ 
+            console.log(error);
+            return res.redirect("back");
+        }else {
             console.log(user);
-            res.redirect("/user/"+user._id+"/messaging");
+            passport.authenticate('local')(req,res,function(){
+                res.redirect("/messaging");
+            });
         };
     });
-})
-
+});
+// login route
+App.get("/login",function(req,res){
+    res.render("./user/login");
+});
+App.post("/login",passport.authenticate("local",{
+    failureRedirect: "/login",
+    successRedirect:"/messaging"
+}),function(req,res){})
 // messaging route
-App.get("/user/:id/messaging",function(req,res){
+App.get("/messaging",function(req,res){
     res.render("./messaging/index");
-})
+});
 
+// logout route
+App.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 
 // app server
 App.listen(process.env.PORT,process.env.IP,function(){
