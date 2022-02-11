2363386dbf339dfded38f226ad1f285077a4d7e9
jaayush27 <aayushjoshi2709@gmail.com>
2022-02-11 20:01:46 +0530

    added login page and friends and messaging routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const req = require('express/lib/request');
+
 const ioClient               = require('socket.io-client'),
       express                = require('express'),
-      App                    = express(),
+      app                    = express(),
       mongoose               = require('mongoose'),
       bodyParser             = require('body-parser'),
       { Server }             = require('socket.io'),
@@ -9,11 +11,13 @@ const ioClient               = require('socket.io-client'),
       localStrategy          = require('passport-local'),
       passportLocalMongoose  = require('passport-local-mongoose');
 
-App.set("view engine","ejs");
+app.set("view engine","ejs");
 // set app to user body parser
-App.use(bodyParser({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // adding public dir to the app
-App.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 // creating user model with mongoose
 mongoose.connect(process.env.databaseURL);
@@ -22,40 +26,126 @@ const userSchema = mongoose.Schema({
     lastName: String,
     email: String,
     password: String,
-    username: String
+    username: String,
+    socketid: String,
+    messages: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message'
+        }
+    ],
+    friends:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 });
-// passport configuration
-userSchema.plugin(passportLocalMongoose);
-var User = mongoose.model("User",userSchema);
-App.use(require('express-session')({
-    secret:"hello world",
-    resave:false,
-    saveUninitialized: false
-}));
-App.use(passport.initialize());
-App.use(passport.session());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-passport.use(new localStrategy(User.authenticate()));
 // creating message model with mongoose
 const messageSchema = mongoose.Schema({
-    from: String,
-    to: String,
+    from:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    to:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    status: String,
     time: String,
     message: String
 });
 var Message = mongoose.model("Message",messageSchema);
+
+// passport configuration
+userSchema.plugin(passportLocalMongoose);
+var User = mongoose.model("User",userSchema);
+app.use(require('express-session')({
+    secret:"hello world",
+    resave:false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new localStrategy(User.authenticate()));
+
+// create message route
+app.post("/user/:id/message",function(req,res){
+    var message = {
+        from: mongoose.Types.ObjectId(req.params.id),
+        to: mongoose.Types.ObjectId(req.body.to),
+        time: req.body.time,
+        message: req.body.message
+    };
+    User.findById(message.to,function(error,toUser){
+        if(error) console.log("To user not found");    
+        Message.create(message,function(error,message){
+            if(error){
+                console.log(error);
+            }else{
+                User.findById(message.from,function(error,fromUser){
+                    if(error){
+                        console.log(error);
+                    }
+                    toUser.messages.push(message._id);
+                    toUser.save();
+                    fromUser.messages.push(message._id);
+                    fromUser.save();
+                })
+            }
+        })
+    })
+});
+// get all messages route
+app.get("/user/:id/message",function(req,res){
+    User.findById(req.params.id).populate('messages').exec(function(error,user){
+        if(error) console.log(error);
+        else{
+            res.send(JSON.stringify(user.messages));
+        }
+    });
+});
+
+// create new friend route
+app.post("/user/:id/friend",function(req,res){
+    User.findById(req.params.id,function(error,user){
+        if(error) console.log(error);
+        else{
+            user.friends.push(mongoose.Types.ObjectId(req.body.friend_id));
+            user.save();
+            res.send("{status:success}");
+        }
+    });
+});
+
+// get all friends route
+app.get("/user/:id/friend",function(req,res){
+    User.findById(req.params.id).populate('friends').exec(function(error,user){
+        if(error) console.log(error);
+        else{
+            var friends = user.friends;
+            friends.forEach(element => {
+                delete element['messages'];
+                console.log(element);
+            });
+            res.send(JSON.stringify(friends));
+        }
+    });
+});
+
 // root route
-App.get("/",function(req,res){
+app.get("/",function(req,res){
     res.render('index')
 })
 
 // new user route
-App.get("/register",function(req,res){
+app.get("/register",function(req,res){
     res.render("./user/new");
 });
 // create user route
-App.post("/register",function(req,res){
+app.post("/register",function(req,res){
     //adding the user to database
     var user = {
         firstName: req.body.firstName,
@@ -73,24 +163,29 @@ App.post("/register",function(req,res){
         };
     });
 });
+
 // login route
-App.get("/login",function(req,res){
+app.get("/login",function(req,res){
     res.render("./user/login");
 });
-App.post("/login",passport.authenticate("local",{
+
+app.post("/login",passport.authenticate("local",{
     failureRedirect: "/login",
     successRedirect:"/messaging"
 }),function(req,res){})
+
 // messaging route
-App.get("/messaging",isLoggedIn,function(req,res){
+app.get("/messaging",isLoggedIn,function(req,res){
     res.render("./messaging/index");
+    console.log(req.user._id);
 });
 
 // logout route
-App.get("/logout",function(req,res){
+app.get("/logout",function(req,res){
     req.logout();
     res.redirect("/");
 });
+
 // check if the user is logged in or not
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
@@ -99,7 +194,8 @@ function isLoggedIn(req,res,next){
         res.redirect('/login');
     }
 }
+
 // app server
-App.listen(process.env.PORT,process.env.IP,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log("server started");
 });
